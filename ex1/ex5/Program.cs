7f3bef38b31e47b40aee615dad2a1c237b1cc2a2
Nastya_krevtka <nastyamkala1278@gmail.com>
2022-12-13 00:52:38 +0300

    ex5 программа с условием

---
+++ ex1/ex5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+
+if (username == "Маша")
+{
+    Console.WriteLine ("Ура, Маша");
+}
+else
+{
+Console.Write ("Привет, ");
+Console.Write (username);
+}
+
